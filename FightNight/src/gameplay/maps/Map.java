477f32f73c3b70dfb07eef368094aac9dbba6a8b
jason <jason@192.168.1.210>
2018-05-06 17:55:02 -0700

    daslkjf

--- FightNight/src/gameplay/maps/Map.java
+++ FightNight/src/gameplay/maps/Map.java
@@ -8,5 +8,14 @@ package gameplay.maps;
  *
  */
 public abstract class Map {
-
+	
+	/**
+	 * Checks if the player is within the bounds of play
+	 * @param x The x coordinate that's being checked
+	 * @param y The y coordinate that's being checked
+	 * @return true if it's in bounds, return false if it isn't
+	 */
+	public abstract boolean isInBounds(double x, double y); 
+	
+	//public abstract isOnTrap(double x, double y); 
 }
