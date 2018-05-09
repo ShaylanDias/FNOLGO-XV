825ce411464330086a9548d7592e06648edbc2c2
Jason <fireblize@gmail.com>
2018-05-08 22:36:27 -0700

    Rectangle hitbox basis

--- FightNight/src/gameplay/maps/StandardMap.java
+++ FightNight/src/gameplay/maps/StandardMap.java
@@ -24,6 +24,29 @@ public class StandardMap extends Map{
 		surface.image(GamePanel.resources.getImage(imageKey), 0, 0, (int) 3000, (int) 3000);
 		surface.noFill();
 		surface.rect(-1500, -1500, 3000, 3000);
+		//Left quadrant tree hitboxes going top down
+		surface.rect(-1440, -1080, 180, 180);
+		surface.rect(-1260, -720, 120, 120);
+		surface.rect(-1440, -540, 120, 120);
+		surface.rect(-1080, -540, 180, 180);
+		surface.rect(-1380, -240, 120, 120);
+		surface.rect(-960, -240, 120, 120);
+		surface.rect(-720, -300, 180, 180);
+		surface.rect(-420, -60, 120, 120);
+		surface.rect(-1140, 0, 180, 180);
+		surface.rect(-780, 180, 120, 120);
+		surface.rect(-1320, 360, 120, 120);
+		surface.rect(-960, 360, 120, 120);
+		surface.rect(-1200, 600, 120, 120);
+		surface.rect(-1380, 780, 120, 120);
+
+
+
+		
+
+
+		
+		
 
 	}
 
