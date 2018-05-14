24a1dfce9398b46f2fb39d726d2809b465060b30
sdias695 <sdias695@PR21.hhs.win.fuhsd.org>
2018-05-14 14:13:16 -0700

    Bennn

--- FightNight/src/networking/backend/GameServer.java
+++ FightNight/src/networking/backend/GameServer.java
@@ -343,11 +343,11 @@ public class GameServer implements NetworkMessenger {
 
 	private void sendClientList() {
 		InetAddress[] connections = getConnectedHosts();
-		String[] con = new String[connections.length];
-		for(int i = 0; i < connections.length; i++) {
-			con[i] = connections[i] + "FNOLGO";
-		}
-		Object[] message = Arrays.copyOf(con, connections.length, Object[].class);
+//		String[] con = new String[connections.length];
+//		for(int i = 0; i < connections.length; i++) {
+//			con[i] = connections[i] + "FNOLGO";
+//		}
+		Object[] message = Arrays.copyOf(connections, connections.length, Object[].class);
 		sendMessage(NetworkDataObject.CLIENT_LIST, message);
 	}
 
