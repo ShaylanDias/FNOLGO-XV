4dd865281446ed202b54c7b52da2eea0717f4080
Ben G <gu.benjamin0@gmail.com>
2018-05-18 23:32:04 -0700

    commented out images; stopped working on it for now

--- FightNight/src/clientside/gui/StartMenu.java
+++ FightNight/src/clientside/gui/StartMenu.java
@@ -12,6 +12,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
@@ -104,16 +105,14 @@ public class StartMenu extends JPanel {
 		startMenu.add(instructionButton, startMenu);
 
 		// BACKGROUND IMAGE
-		try {
-			backgroundImage = ImageIO.read(new File("data/Background.jpg"));
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		JLabel background = new JLabel();
-		background.setIcon(new ImageIcon(backgroundImage));
-
-		System.out.println(background.getHorizontalAlignment());
+//		try {
+//			backgroundImage = ImageIO.read(new File("data/Background.jpg"));
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		JLabel background = new JLabel();
+//		background.setIcon(new ImageIcon(backgroundImage));
 
 		// startMenu.add(background);
 
